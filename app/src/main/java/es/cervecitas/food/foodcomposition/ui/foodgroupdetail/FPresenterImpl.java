d8abbb173e9158c2989fbe94edb9df002c7b98b0
DESKTOP-C9722MU\luism <luismu78@gmail.com>
2017-05-27 09:06:23 +0200

    removed search from FoodDetail bugfix

--- app/src/main/java/es/cervecitas/food/foodcomposition/ui/foodgroupdetail/FPresenterImpl.java
+++ app/src/main/java/es/cervecitas/food/foodcomposition/ui/foodgroupdetail/FPresenterImpl.java
@@ -2,15 +2,18 @@ package es.cervecitas.food.foodcomposition.ui.foodgroupdetail;
 
 import android.content.Context;
 
-import es.cervecitas.food.foodcomposition.app.FoodCompositionApplication;
-import es.cervecitas.food.foodcomposition.network.BedcaApi;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
+import es.cervecitas.food.foodcomposition.app.FoodCompositionApplication;
+import es.cervecitas.food.foodcomposition.network.BedcaApi;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
@@ -51,17 +54,29 @@ public class FPresenterImpl implements FPresenter {
 
         String query = getHeaders() + "<foodquery>" + getQueryType() + getSelection() + condition1 + condition2 + getOrder() + "</foodquery>";
 
-        bedcaApi
+        compositeDisposable.add(bedcaApi
                 .getFoodGroupDetail(RequestBody.create(MediaType.parse("text/xml"), query))
                 .subscribeOn(Schedulers.io())
+                .onErrorReturn(new Function<Throwable, F_ListItems>() {
+                    @Override
+                    public F_ListItems apply(@NonNull Throwable throwable) throws Exception {
+                        return new F_ListItems();
+                    }
+                })
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<F_ListItems>() {
                     @Override
                     public void accept(@NonNull F_ListItems f_listItems) throws Exception {
-                        view.onDataLoaded(f_listItems.getFoodResponse());
+                        List<Food> foods = new ArrayList<>();
+
+                        if (f_listItems.getFoodResponse() != null) {
+                            foods = f_listItems.getFoodResponse();
+                        }
+
+                        view.onDataLoaded(foods);
                         view.hideLoading();
                     }
-                });
+                }));
 
     }
 
